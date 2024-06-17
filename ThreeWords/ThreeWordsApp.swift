3a786a4dc96140d09ec682a6d97d219fabf2120c
Ariq Ramdhany <ariq.ramdhany@ittend.com>
2024-06-17 20:19:17 +0900

    Initial Commit

---
+++ ThreeWords/ThreeWordsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThreeWordsApp.swift
+//  ThreeWords
+//
+//  Created by Ariq Ramdhany on 17/06/24.
+//
+
+import SwiftUI
+
+@main
+struct ThreeWordsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
