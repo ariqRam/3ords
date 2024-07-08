a022b345226db9fe8957d7c02eaed894be9198c5
Ariq Ramdhany <ariq.ramdhany@ittend.com>
2024-07-09 08:03:18 +0900

    Added card view

--- ThreeWords/ContentView.swift
+++ ThreeWords/ContentView.swift
@@ -7,15 +7,52 @@
 
 import SwiftUI
 
+extension Color {
+    static var random: Color {
+        return Color(
+            red: .random(in: 0...1),
+            green: .random(in: 0...1),
+            blue: .random(in: 0...1)
+        )
+    }
+}
+
 struct ContentView: View {
     @State private var records: [DictRecord] = []
     var body: some View {
-        List(records) { record in
-            VStack(alignment: .leading, content: {
-                Text(record.reading)
-                Text(record.meaning)
-            })
+        ScrollView(.horizontal) {
+                HStack{
+                    ForEach(records) { record in
+                        ZStack {
+                            RoundedRectangle(cornerSize: CGSize(width: 20, height: 10))
+                                .foregroundColor(Color.primaryBg)
+                            VStack{
+                                Text(record.word)
+                                    .containerRelativeFrame(.horizontal)
+                                    .font(.title)
+                                    .fontWeight(.heavy)
+                                Text(record.reading)
+                                    .font(.system(size: 15))
+                                Text(record.meaning)
+                                    .font(.system(size:20))
+                            }
+                            .foregroundColor(.white)
+                        }
+                        .frame(height: 350)
+                        .scrollTransition { content, phase in
+                            content
+                                .opacity(phase.isIdentity ? 1.0 : 0.5)
+                                .scaleEffect(phase.isIdentity ? 1.0 : 0.9)
+                        }
+                    }
+                }
+                
+            
         }
+        .scrollTargetLayout(isEnabled: true)
+        .scrollTargetBehavior(.viewAligned)
+        .safeAreaPadding(.horizontal, 40)
+        .contentMargins(8, for: .scrollContent)
         .onAppear{
             if ProcessInfo.processInfo.environment["XCODE_RUNNING_FOR_PREVIEWS"] == "1" {
                 self.records = DictRecord.sampleData
